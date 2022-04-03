6bbac3a0c7eb71840027fa3517006a4244aad2a4
19127312 <19127312@student.hcmus.edu.vn>
2022-04-03 08:22:20 +0700

    [19127312]-Init Project Again

--- BookStore/MainWindow.xaml.cs
+++ BookStore/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BookStore.MyUserControl;
+using Fluent;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +21,57 @@ namespace BookStore
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : RibbonWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void exitMenu_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void excelImportButton_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deleteCategorytButton_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addCategorytButton_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addBookButton_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void editBookButton_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deleteBookButton_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RibbonWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var screens = new ObservableCollection<TabItem>()
+             {
+            new TabItem() { Content = new MasterDataUserControl()},
+            new TabItem() { Content = new SaleUserControl()},
+            new TabItem() { Content = new ReportUserControl()}
+            };
+            tabs.ItemsSource = screens;
         }
     }
 }
