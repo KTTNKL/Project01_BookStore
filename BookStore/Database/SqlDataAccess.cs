767c689a4ffb3d862ec532cc5fcdbc75a3539202
luong07734 <le.ky.luong@gmail.com>
2022-04-03 10:49:36 +0700

    [19127208] crud

--- BookStore/Database/SqlDataAccess.cs
+++ BookStore/Database/SqlDataAccess.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,61 @@ namespace BookStore.Database
 {
     internal class SqlDataAccess
     {
+        private SqlConnection _connection;
+        public SqlDataAccess(string connectionString)
+        {
+            _connection = new SqlConnection(connectionString);
+        }
+
+        public bool CanConnect()
+        {
+            bool result = true;
+
+            try
+            {
+                _connection.Open();
+                _connection.Close();
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        public void Connect()
+        {
+            _connection.Open();
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            var sql = "select * from Category where category_id=@CatId";
+            var command = new SqlCommand(sql, _connection);
+
+            command.Parameters.Add("CatId", SqlDbType.Int).Value = id;
+
+            var reader = command.ExecuteReader();
+
+            Category result = null;
+
+            if (reader.Read()) // ORM - Object relational mapping
+            {
+                var catId = (int)reader["category_id"];
+                var catName = (string)reader["category_name"];
+
+                result = new Category()
+                {
+                    ID = catId,
+                    Name = catName,
+                };
+            }
+
+            return result;
+        }
+    }
+
+
+
     }
 }
--- BookStore/MainWindow.xaml.cs
+++ BookStore/MainWindow.xaml.cs
@@ -1,12 +1,9 @@
-﻿using Aspose.Cells;
-using BookStore.Database;
+﻿using BookStore.Database;
 using BookStore.MyUserControl;
 using Fluent;
-using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,8 +24,6 @@ namespace BookStore
     /// </summary>
     public partial class MainWindow : RibbonWindow
     {
-        List<Category> _categories = null;
-        
         public MainWindow()
         {
             InitializeComponent();
@@ -41,68 +36,7 @@ namespace BookStore
 
         private void excelImportButton_Clicked(object sender, RoutedEventArgs e)
         {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-            _categories = new List<Category>();
-
-            var screen = new OpenFileDialog();
-
-            if (screen.ShowDialog() == true)
-            {
-                string filename = screen.FileName;
-                Debug.WriteLine(filename);
-
-                var workbook = new Workbook(filename);
-
-                var tabs = workbook.Worksheets;
-                // In ra các tab để debug
-                foreach (var tab in tabs)
-                {
-                    Category cat = new Category()
-                    {
-                        Name = tab.Name,
-                        Books = new List<Book>()
-                    };
-
-                    // Bắt đầu từ ô B3
-                    var column = 'C';
-                    var row = 4;
-
-                    var cell = tab.Cells[$"{column}{row}"];
-
-                    while (cell.Value != null)
-                    {
-                        string name = cell.StringValue;
-                        string author = tab.Cells[$"D{row}"].StringValue;
-                        int publicYear = tab.Cells[$"E{row}"].IntValue;
-                        string bookCover = tab.Cells[$"F{row}"].StringValue;
-                        int purchasePrice = tab.Cells[$"G{row}"].IntValue;
-                        int sellingPrice = tab.Cells[$"H{row}"].IntValue;
-                        int stockNumer = tab.Cells[$"I{row}"].IntValue;
-                        int sellingNumber = tab.Cells[$"J{row}"].IntValue;
-
-                        Console.WriteLine(bookCover);
-
-                        var p = new Book(name, author, publicYear, bookCover, purchasePrice, sellingPrice, stockNumer, sellingNumber);
-                        cat.Books.Add(p);
-
-                        row++;
-                        cell = tab.Cells[$"{column}{row}"];
-                    }
-
-                    _categories.Add(cat); // Model
-                }
-
-            }
-
-            for(int i = 0; i < _categories.Count(); i++)
-            {
-                for(int j = 0; j < _categories[i].Books.Count(); j++)
-                {
-                    Debug.WriteLine($"{_categories[i].Books[j].name} {_categories[i].Books[j].author} {_categories[i].Books[j].publicYear} {_categories[i].Books[j].bookCover} {_categories[i].Books[j].purchasePrice} {_categories[i].Books[j].sellingPrice} {_categories[i].Books[j].stockNumer} {_categories[i].Books[j].sellingNumber}");
-                }
-                
-            }
         }
 
         private void deleteCategorytButton_Clicked(object sender, RoutedEventArgs e)
@@ -139,6 +73,21 @@ namespace BookStore
             new TabItem() { Content = new ReportUserControl()}
             };
             tabs.ItemsSource = screens;
+
+            string? connectionString = AppConfig.ConnectionString();
+            var dao = new SqlDataAccess(connectionString!);
+            if (dao.CanConnect())
+            {
+                dao.Connect();
+                // Thao tác với CSDL ở đây
+
+                MessageBox.Show("Connect to db");
+
+            }
+            else
+            {
+                MessageBox.Show("Cannot connect to db");
+            }
         }
     }
 }
