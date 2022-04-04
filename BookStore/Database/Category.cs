649973e5d470f9b764dcb1ac5795760ee519ef81
luong07734 <le.ky.luong@gmail.com>
2022-04-04 10:48:46 +0700

    Revert "[19127208] Add button"
    This reverts commit d74bc09e6e48e2fc15504a3ad4073ef63627a323.

--- BookStore/Database/Category.cs
+++ BookStore/Database/Category.cs
@@ -1,23 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BookStore.Database
 {
-    public class Category: INotifyPropertyChanged, ICloneable
+    public class Category
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public List<Book> Books { get; set; }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        public object Clone()
-        {
-            return MemberwiseClone();
-        }
     }
 }
