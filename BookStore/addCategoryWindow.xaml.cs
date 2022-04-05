6db428b523cc100f348362c6e82315ae25feb179
TrungNam <huynhtrungnam2001@gmail.com>
2022-04-05 17:11:00 +0700

    [19127478] Add/delete Category

---
+++ BookStore/addCategoryWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BookStore.Database;
+
+namespace BookStore
+{
+    /// <summary>
+    /// Interaction logic for addCategoryWindow.xaml
+    /// </summary>
+    public partial class addCategoryWindow : Window
+    {
+
+        public Category newCat { get; set; }
+        public addCategoryWindow()
+        {
+            InitializeComponent();
+            newCat = new Category();
+            this.DataContext = newCat;
+        }
+
+        private void okBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
---
+++ BookStore/deleteCategoryWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using BookStore.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookStore
+{
+    /// <summary>
+    /// Interaction logic for deleteCategoryWindow.xaml
+    /// </summary>
+    public partial class deleteCategoryWindow : Window
+    {
+        public List<Category> category;
+        public int index = -1;
+        public deleteCategoryWindow(List<Category> _categories)
+        {
+            category = _categories;
+            InitializeComponent();
+            
+        }
+
+        private void okBtn_Click(object sender, RoutedEventArgs e)
+        {
+            index = deleteCategoriesComboBox.SelectedIndex;
+            DialogResult = true;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            deleteCategoriesComboBox.ItemsSource = category;
+        }
+    }
+}
