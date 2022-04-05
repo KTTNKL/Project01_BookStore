6c75061c8ecba9a7e33005705d249f8f4c657739
19127312 <19127312@student.hcmus.edu.vn>
2022-04-05 21:36:24 +0700

    [19127312] FixBug

--- BookStore/MainWindow.xaml.cs
+++ BookStore/MainWindow.xaml.cs
@@ -27,126 +27,22 @@ namespace BookStore
     /// </summary>
     public partial class MainWindow : RibbonWindow
     {
-        List<Category> _categories = null;
         Business _bus = null;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void exitMenu_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-
-        }
-
-        private void excelImportButton_Clicked(object sender, RoutedEventArgs e)
-        {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-            _categories = new List<Category>();
-
-            var screen = new OpenFileDialog();
-
-            if (screen.ShowDialog() == true)
-            {
-                string filename = screen.FileName;
-
-                var workbook = new Workbook(filename);
-
-                var tabs = workbook.Worksheets;
-                // In ra các tab để debug
-                foreach (var tab in tabs)
-                {
-                    Category cat = new Category()
-                    {
-                        Name = tab.Name,
-                        Books = new List<Book>()
-                    };
-
-                    // Bắt đầu từ ô B3
-                    var column = 'C';
-                    var row = 4;
-
-                    var cell = tab.Cells[$"{column}{row}"];
-
-                    while (cell.Value != null)
-                    {
-                        string name = cell.StringValue;
-                        string author = tab.Cells[$"D{row}"].StringValue;
-                        int publicYear = tab.Cells[$"E{row}"].IntValue;
-                        string bookCover = tab.Cells[$"F{row}"].StringValue;
-                        int purchasePrice = tab.Cells[$"G{row}"].IntValue;
-                        int sellingPrice = tab.Cells[$"H{row}"].IntValue;
-                        int stockNumer = tab.Cells[$"I{row}"].IntValue;
-                        int sellingNumber = tab.Cells[$"J{row}"].IntValue;
-
-                        Console.WriteLine(bookCover);
-
-                        var p = new Book(name, author, publicYear, bookCover, purchasePrice, sellingPrice, stockNumer, sellingNumber);
-                        cat.Books.Add(p);
-
-                        row++;
-                        cell = tab.Cells[$"{column}{row}"];
-                    }
-
-                    _categories.Add(cat); // Model
-                }
-
-
-            }
-            _bus.deleteTable("Book");
-            _bus.deleteTable("Category");
-
-            for (int i = 0; i < _categories.Count(); i++)
-            {
-                _bus.insertCategory(_categories[i].Name);
-                int id = _bus.getCategoryID(_categories[i].Name);
-
-                for (int j = 0; j < _categories[i].Books.Count(); j++)
-                {
-
-                    _bus.insertBook(_categories[i].Books[j].name, _categories[i].Books[j].author, _categories[i].Books[j].publicYear, _categories[i].Books[j].bookCover, _categories[i].Books[j].purchasePrice, _categories[i].Books[j].sellingPrice, _categories[i].Books[j].stockNumer, _categories[i].Books[j].sellingNumber, id);
-                }
-
-            }
-        }
-
-        private void deleteCategorytButton_Clicked(object sender, RoutedEventArgs e)
-        {
-            var screen = new MasterDataUserControl();
-            screen.HandleParentEvent(MasterDataUserControl.MasterDataAction.DeleteSelectedCategory);
-        }
-
-        private void addCategorytButton_Clicked(object sender, RoutedEventArgs e)
-        {
-            var screen = new MasterDataUserControl();
-            screen.HandleParentEvent(MasterDataUserControl.MasterDataAction.AddNewCategory);
-
-        }
-
-        private void addBookButton_Clicked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void editBookButton_Clicked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void deleteBookButton_Clicked(object sender, RoutedEventArgs e)
-        {
-
-        }
         private void RibbonWindow_Loaded(object sender, RoutedEventArgs e)
         {
             var screens = new ObservableCollection<TabItem>()
              {
-            new TabItem() { Content = new MasterDataUserControl()},
-            new TabItem() { Content = new SaleUserControl()},
-            new TabItem() { Content = new ReportUserControl()}
+            new TabItem() { Header="Master Data", Content = new MasterDataUserControl()},
+            new TabItem() { Header="Sale",Content = new SaleUserControl()},
+            new TabItem() { Header="Report",Content = new ReportUserControl()}
             };
             tabs.ItemsSource = screens;
+            tabs.SelectedIndex = 0;
 
             string? connectionString = AppConfig.ConnectionString();
             var dao = new SqlDataAccess(connectionString!);
